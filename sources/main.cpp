772dc663b1e0a2b8ab3252fce2bf29827b9fcb5f
kenyakikuchi <kikuchi15607@gmail.com>
2017-10-29 01:34:39 +0900

    add

---
+++ sources/main.cpp
@@ -0,0 +1,16 @@
+#include <resources.h>
+#include <GL/glut.h>
+
+void display(void)
+{
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
